a20e57423af9136f63d68211d46b41b0c7975cb5
crispyhambun <shinigami709k@gmail.com>
2024-10-13 17:20:13 +0530

    project structure mafe

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+import exception
+import sys
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y _%H_%M_%S')}.log"
+logs_path =os.path.join(os.getcwd(),"Logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[(%(asctime)s] -%(lineno)s  %(name)s - %(levelname)s - %(message)s)",
+    level=logging.INFO
+)
+
