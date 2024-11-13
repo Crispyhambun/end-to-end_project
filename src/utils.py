57f5be90e697374658441bd513cd4f168d6f4e6a
crispyhambun <shinigami709k@example.com>
2024-11-13 13:51:46 +0530

    prediction pipleine

--- src/utils.py
+++ src/utils.py
@@ -33,5 +33,9 @@ def eval_model(x_train,y_train,x_test,y_test,models):
     except Exception as e:
         raise Custom_Exception(e,sys)
 
-
-
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)  
+    except Exception as e:
+        raise Custom_Exception(e,sys)
